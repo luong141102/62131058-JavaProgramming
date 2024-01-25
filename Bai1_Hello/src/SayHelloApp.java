04fbd211f14959dc961eb7100cd4dda368fb9644
luong141102 <ngocluong3959@gmail.com>
2024-01-25 20:35:18 +0700

    Vi du
    .

---
+++ Bai1_Hello/src/SayHelloApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class SayHelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao. Toi la sinh vien NTU!");
+		System.out.println("Trần Ngọc Lương");
+	}
+
+}
