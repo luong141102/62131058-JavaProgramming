c20abd88d078a748b873ae39e5d18b1a6a895b48
luong141102 <ngocluong3959@gmail.com>
2024-02-22 10:44:15 +0700

    bai3
    d

--- Lab3/src/Bai3.java
+++ Lab3/src/Bai3.java
@@ -1,6 +1,9 @@
 /**
  * 
  */
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * 
@@ -12,7 +15,24 @@ public class Bai3 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập số phần tử của mảng: ");
+        int n = scanner.nextInt();
+        // khởi tạo mảng arr
+        int[] arr = new int[n];
+        System.out.print("Nhập các phần tử của mảng: \n");
+        for (int i = 0; i < n; i++) {
+            System.out.printf("a[%d] = ", i);
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.print("sắp xếp phần tử của mảng: ");
+        show(arr);
+        
 	}
-
+	public static void show(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%d ", arr[i]);
+        }
+    }
 }
